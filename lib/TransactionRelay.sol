40b91f3ff349f0d7c202c6be8a9885dc433856d2
Ian Pierce <ianpierce42@gmail.com>
2025-05-28 13:00:41 -0500

    feat: changed import

--- lib/TransactionRelay.sol
+++ lib/TransactionRelay.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GNU-3.0
 pragma solidity ^0.8.23;
 
-import "./IRelay.sol";
+import {IRelay} from "./IRelay.sol";
 
 /// @author Ian Pierce
 contract TransactionRelay is IRelay {
