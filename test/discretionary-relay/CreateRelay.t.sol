062c0e2ba3e89b7c01ef0dc2d52bf4d099360c89
Ian Pierce <ianpierce42@gmail.com>
2025-06-29 10:50:52 -0500

    moved test files; new contract test for isDisputable function

--- test/discretionary-relay/CreateRelay.t.sol
+++ test/discretionary-relay/CreateRelay.t.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.8.20;
 
-import "../lib/IRelay.sol";
-import {CommonBase} from "../lib/forge-std/src/Base.sol";
-import {DiscretionaryRelay} from "../lib/DiscretionaryRelay.sol";
-import {StdAssertions} from "../lib/forge-std/src/StdAssertions.sol";
-import {StdChains} from "../lib/forge-std/src/StdChains.sol";
-import {StdCheats, StdCheatsSafe} from "../lib/forge-std/src/StdCheats.sol";
-import {StdUtils} from "../lib/forge-std/src/StdUtils.sol";
-import {Test} from "../lib/forge-std/src/Test.sol";
+import "../../lib/IRelay.sol";
+import {CommonBase} from "../../lib/forge-std/src/Base.sol";
+import {DiscretionaryRelay} from "../../lib/DiscretionaryRelay.sol";
+import {StdAssertions} from "../../lib/forge-std/src/StdAssertions.sol";
+import {StdChains} from "../../lib/forge-std/src/StdChains.sol";
+import {StdCheats, StdCheatsSafe} from "../../lib/forge-std/src/StdCheats.sol";
+import {StdUtils} from "../../lib/forge-std/src/StdUtils.sol";
+import {Test} from "../../lib/forge-std/src/Test.sol";
 
-contract CreateDiscretionaryRelayTest is Test {
+contract DiscretionaryRelayCreateRelayTest is Test {
     DiscretionaryRelay public relay;
     address public alice = address(0xA11CE);
     address public bob = address(0xB0B);
