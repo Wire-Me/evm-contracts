8476589fe380f3926143bbbc6968785f3356f814
Ian Pierce <ianpierce42@gmail.com>
2025-06-30 11:53:55 -0500

    added an owner which deploys the relay

--- test/discretionary-relay/DiscretionaryRelayTest.t.sol
+++ test/discretionary-relay/DiscretionaryRelayTest.t.sol
@@ -7,12 +7,14 @@ import {DiscretionaryRelay} from "../../lib/DiscretionaryRelay.sol";
 
 abstract contract DiscretionaryRelayTest is Test {
     DiscretionaryRelay public relay;
+    address public owner = address(0x0bcE5);
     address public alice = address(0xA11CE);
     address public bob = address(0xB0B);
     address public dee = address(0xDEE);
     uint public currentBlockTimestamp = 4102444800; // 2100/01/01 00:00:00 GMT
 
     function setUp() public virtual {
+        vm.prank(owner);
         relay = new DiscretionaryRelay("ETH", 100);
         vm.warp(currentBlockTimestamp); // Set the current block timestamp
 
