84390ed3860d13045f2c0516247c18b442561872
Ian Pierce <ianpierce42@gmail.com>
2025-10-09 10:04:09 +0300

    new proxy contracts for each fx escrow erc20 deployed contract for usdc and usdt

---
+++ lib/fx-contracts/ProxyFxEscrowERC20USDC.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GNU-3.0
+pragma solidity ^0.8.30;
+
+contract ProxyFxEscrowERC20USDC {
+    address public implementation;
+    address public admin;
+
+    constructor(address _impl, address _admin) {
+        admin = _admin;
+        implementation = _impl;
+    }
+
+    function setImplementation(address _impl) external {
+        // add access control if you want
+        require(msg.sender == admin, "Only admin account can call this function");
+        implementation = _impl;
+    }
+
+    fallback() external payable {
+        address impl = implementation;
+        require(implementation != address(0), "No implementation");
+
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let success := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+
+            switch success
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+
+    receive() external payable {}
+}
