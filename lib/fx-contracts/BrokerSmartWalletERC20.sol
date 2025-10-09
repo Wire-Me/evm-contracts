8d401624db3f2ae7e823bdcf9e233e42d44e7de4
Ian Pierce <ianpierce42@gmail.com>
2025-10-09 08:47:47 +0300

    commented out and deprecated contracts

--- lib/fx-contracts/BrokerSmartWalletERC20.sol
+++ lib/fx-contracts/BrokerSmartWalletERC20.sol
@@ -3,20 +3,20 @@ pragma solidity ^0.8.30;
 
 import {SmartWalletERC20} from "./SmartWalletERC20.sol";
 
-
+// Deprecated
 contract BrokerSmartWalletERC20 is SmartWalletERC20 {
     constructor(address _escrowContractAddress, address _authorizedUserExternalWallet)
         SmartWalletERC20(_escrowContractAddress, _authorizedUserExternalWallet) {}
 
-    function createOffer(
-        address _escrowAccount,
-        uint _escrowIndex
-    ) external {
-        require(_escrowAccount != address(0), "Escrow account cannot be zero address");
-        escrowContract.createOffer(_escrowAccount, _escrowIndex);
-    }
-
-    function withdrawEscrow(address escrowAccount, uint escrowIndex) external {
-        escrowContract.withdrawEscrowAfterCompletion(escrowAccount, escrowIndex);
-    }
+//    function createOffer(
+//        address _escrowAccount,
+//        uint _escrowIndex
+//    ) external {
+//        require(_escrowAccount != address(0), "Escrow account cannot be zero address");
+//        escrowContract.createOffer(_escrowAccount, _escrowIndex);
+//    }
+//
+//    function withdrawEscrow(address escrowAccount, uint escrowIndex) external {
+//        escrowContract.withdrawEscrowAfterCompletion(escrowAccount, escrowIndex);
+//    }
 }
--- lib/fx-contracts/UserSmartWalletERC20.sol
+++ lib/fx-contracts/UserSmartWalletERC20.sol
@@ -3,32 +3,32 @@ pragma solidity ^0.8.30;
 
 import {SmartWalletERC20} from "./SmartWalletERC20.sol";
 
-
+// Deprecated
 contract UserSmartWalletERC20 is SmartWalletERC20 {
     constructor(address _escrowContractAddress, address _admin)
         SmartWalletERC20(_escrowContractAddress, _admin) {}
 
-    function transferFundsAndCreateEscrow(uint _amount) external {
-        erc20TokenContract.transfer(address(escrowContract), _amount);
-
-        escrowContract.createEscrow(_amount);
-    }
-
-    function linkOfferToEscrow(uint _escrowIndex, address _offerAccount, uint _offerIndex) external {
-        require(_offerAccount != address(0), "Offer account cannot be zero address");
-
-        escrowContract.linkOfferToEscrow(_escrowIndex, _offerAccount, _offerIndex);
-    }
-
-    function extendEscrow(uint escrowIndex) external {
-        escrowContract.extendEscrow(escrowIndex);
-    }
-
-    function withdrawEscrowEarly(uint escrowIndex) external {
-        escrowContract.withdrawEscrowEarly(escrowIndex);
-    }
-
-    function withdrawEscrow(uint escrowIndex) external {
-        escrowContract.withdrawEscrowAfterReturn(escrowIndex);
-    }
+//    function transferFundsAndCreateEscrow(uint _amount) external {
+//        erc20TokenContract.transfer(address(escrowContract), _amount);
+//
+//        escrowContract.createEscrow(_amount);
+//    }
+//
+//    function linkOfferToEscrow(uint _escrowIndex, address _offerAccount, uint _offerIndex) external {
+//        require(_offerAccount != address(0), "Offer account cannot be zero address");
+//
+//        escrowContract.linkOfferToEscrow(_escrowIndex, _offerAccount, _offerIndex);
+//    }
+//
+//    function extendEscrow(uint escrowIndex) external {
+//        escrowContract.extendEscrow(escrowIndex);
+//    }
+//
+//    function withdrawEscrowEarly(uint escrowIndex) external {
+//        escrowContract.withdrawEscrowEarly(escrowIndex);
+//    }
+//
+//    function withdrawEscrow(uint escrowIndex) external {
+//        escrowContract.withdrawEscrowAfterReturn(escrowIndex);
+//    }
 }
