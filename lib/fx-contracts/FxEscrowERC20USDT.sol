4bdf4299908fa5b6c5269165e3e25d5a2beccc5a
Ian Pierce <ianpierce42@gmail.com>
2025-10-09 10:38:23 +0300

    added contracts for both currency types

---
+++ lib/fx-contracts/FxEscrowERC20USDT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GNU-3.0
+pragma solidity ^0.8.30;
+
+import {FxEscrowERC20} from "./FxEscrowERC20.sol";
+
+contract FxEscrowERC20USDT is FxEscrowERC20 {
+    constructor(address _erc20TokenAddress, address _admin)
+    FxEscrowERC20(_erc20TokenAddress, _admin) {}
+}
