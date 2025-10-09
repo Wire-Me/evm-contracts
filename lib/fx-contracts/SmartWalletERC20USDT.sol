97f05a6f214ee59e760d6561a70f61d9ca73e3c0
Ian Pierce <ianpierce42@gmail.com>
2025-10-09 11:35:10 +0300

    added smart wallet contracts for USDC and USDT

---
+++ lib/fx-contracts/SmartWalletERC20USDT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GNU-3.0
+pragma solidity ^0.8.30;
+
+import {SmartWalletERC20} from "./SmartWalletERC20.sol";
+
+contract SmartWalletERC20USDT is SmartWalletERC20 {
+    constructor(address _escrowContractAddress, address _admin)
+    SmartWalletERC20(_escrowContractAddress, _admin) {}
+}
