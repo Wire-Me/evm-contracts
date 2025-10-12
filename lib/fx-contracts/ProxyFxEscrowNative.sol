857fe797d61c26caf246c85a31ce48394d999aa4
Ian Pierce <ianpierce42@gmail.com>
2025-10-12 09:59:34 +0300

    created common abstract contract for all fx escrow proxies

--- lib/fx-contracts/ProxyFxEscrowNative.sol
+++ lib/fx-contracts/ProxyFxEscrowNative.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.30;
 
 import {ProxyFxEscrow} from "./ProxyFxEscrow.sol";
 
-contract ProxyFxEscrowERC20USDT is ProxyFxEscrow {
+contract ProxyFxEscrowNative is ProxyFxEscrow {
     constructor(address _impl, address _admin) ProxyFxEscrow(_impl, _admin) {}
 }
