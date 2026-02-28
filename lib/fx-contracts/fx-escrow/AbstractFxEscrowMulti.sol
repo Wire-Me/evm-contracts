174b79f44776738e55cf51fd9264a2267bc34f69
ip10x <ianpierce42@gmail.com>
2026-02-28 11:15:49 -0600

    moves function to implementation contract from proxy contract

--- lib/fx-contracts/fx-escrow/AbstractFxEscrowMulti.sol
+++ lib/fx-contracts/fx-escrow/AbstractFxEscrowMulti.sol
@@ -176,6 +176,10 @@ abstract contract AbstractFxEscrowMulti is FxEscrowMultiStorage {
         return _implementation;
     }
 
+    function setImplementation(address _impl) external onlyAdmin {
+        _implementation = _impl;
+    }
+
     function createEscrow(bytes32 _token, uint _amount) external payable onlyAuthorizedUsers {
         if (_token == NATIVE_TOKEN) {
             require(msg.value == _amount, "Incorrect native amount sent");
